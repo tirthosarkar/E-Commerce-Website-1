70e6c341e1f01af5e14d83fba493d301e080abc9
tirthosarkar <tirthosarkar1@gmail.com>
2026-07-28 20:47:26 +0600

    Added Footer

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,6 @@ function App() {
   return (
     <>
       <Header /> 
-      <div className="min-h-80 flex flex-col items-center justify-center">
-        <h2 className="text-2xl font-bold">Welcom to MERN stack ecommerce series</h2>
-        <p className="text-sm">This is a real world ecommerce project that will develop on MERN. We will build step by step</p>
-      </div>
       <Footer />
     </>
   )
--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className=''>
+    <div className='py-3 bg-gray-200'>
       <p className='text-center '>Copyright all rights reserved</p>
     </div>
   )
