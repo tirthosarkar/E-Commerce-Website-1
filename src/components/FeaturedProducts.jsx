db74e59df17b6e69f0d813ef925def26b1aa84f6
tirthosarkar <tirthosarkar1@gmail.com>
2026-08-04 00:15:28 +0600

    Added Feautureproduct

---
+++ src/components/FeaturedProducts.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const FeaturedProducts = () => {
+  return (
+    <div className='max-w-360 mx-auto lg:px-8 px-5 '>
+      <h2 className="text-green-400 text-4xl font-medium pb-10">
+        Featured <span className="text-gray-700">Products</span>
+      </h2>
+    </div>
+  );
+}
+
+export default FeaturedProducts
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import Header from '../components/layouts/Header'
 import Footer from '../components/layouts/Footer'
 import Hero from '../components/Hero'
+import FeaturedProducts from '../components/FeaturedProducts'
+
 
 const Home = () => {
   return (
     <>
       <Header />
-      <h1>Home</h1>
       <Hero />
+      <FeaturedProducts />
       <Footer />
     </>
   )
