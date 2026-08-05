29ad272afb56e65e132db2d922d0b09ba0060cd6
tirthosarkar <tirthosarkar1@gmail.com>
2026-08-05 23:55:53 +0600

    Added hero, heder, footer header link number added copy right

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -5,7 +5,7 @@ import Register from './../../pages/Register';
 
 const Footer = () => {
   return (
-    <div className="max-w-360  mx-auto bg-green-300 lg:px-8 px-5">
+    <div className="max-w-360 mx-auto bg-green-300 lg:px-8 px-5">
       <div className="grid grid-cols-1 gap-y-5 lg:grid-cols-3 rounded-lg p-10">
         <Link to="/" className="flex items-center">
           <span className="text-3xl text-white font-bold">X</span>
@@ -54,6 +54,9 @@ const Footer = () => {
             </a>
           </div>
         </div>
+      </div>
+      <div className="text-center py-5 text-gray-700">
+        &copy; {new Date().getFullYear()} ECOMMERCE. All rights reserved.
       </div>
     </div>
   );
