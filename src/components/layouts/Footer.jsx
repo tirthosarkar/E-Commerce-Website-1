9317fd429029df0baed691232d02c0267ca18ccb
tirthosarkar <tirthosarkar1@gmail.com>
2026-08-05 23:53:45 +0600

    Added hero, heder, footer header link number

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -5,17 +5,27 @@ import Register from './../../pages/Register';
 
 const Footer = () => {
   return (
-    <div className="max-w-360 mx-auto bg-green-300 lg:px-8 px-5">
-      <div className="grid grid-cols-1 lg:grid-cols-3 rounded-lg p-10">
+    <div className="max-w-360  mx-auto bg-green-300 lg:px-8 px-5">
+      <div className="grid grid-cols-1 gap-y-5 lg:grid-cols-3 rounded-lg p-10">
         <Link to="/" className="flex items-center">
           <span className="text-3xl text-white font-bold">X</span>
           <span className="text-3xl text-slate-800 font-bold">ECOMM</span>
         </Link>
         <p>Your destination for all your shopping needs.</p>
-        <div className='flex flex-col'>
-          <h2 className='text-xl text-gray-800 font-bold mb-3'>Quick Links</h2>
-          <Link>Login</Link>
-          <Link>Register</Link>
+        <div className="flex flex-col">
+          <h2 className="text-xl text-gray-800 font-bold mb-3">Quick Links</h2>
+          <Link className="hover:underline" to="/login">
+            Login
+          </Link>
+          <Link className="hover:underline" to="/register">
+            Register
+          </Link>
+        </div>
+        <div className="flex flex-col">
+          <h2 className="text-xl text-gray-800 font-bold mb-3">Contact</h2>
+          <Link className="hover:underline">+8801712345678</Link>
+          <Link className="hover:underline">info@ecommerce.com</Link>
+          <p>Dummy address 123</p>
         </div>
         <div className="space-y-3">
           <h2 className="text-2xl text-gray-800 font-semibold">About Us</h2>
