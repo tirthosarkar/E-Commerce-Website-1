69f60653898e678db6476d021df4d67881584111
tirthosarkar <tirthosarkar1@gmail.com>
2026-08-08 22:49:23 +0600

    Added layout.jsx

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Header from '../components/layouts/Header';
+import Footer from '../components/layouts/Footer';
 
 const Shop = () => {
   return (
-    <div>Shop</div>
+    <>
+      <Header />
+      
+      <Footer />
+
+    </>
   )
 }
 
