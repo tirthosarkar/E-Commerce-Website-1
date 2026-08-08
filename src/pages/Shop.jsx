f0d4730c73f6fb7524725b85950b7fd44d648d08
tirthosarkar <tirthosarkar1@gmail.com>
2026-08-08 23:57:56 +0600

    Added shop.jsx py

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -10,7 +10,7 @@ const Shop = () => {
       <Layout>
         {/*Breadcrumb*/}
 
-        <div className='max-w-360 lg:px-8 px-5 mx-auto'>
+        <div className='max-w-360 lg:px-8 px-5 mx-auto py-5'>
           <div className="flex gap-x-1 items-center text-sm  ">
             <Link to="/">Home</Link>
             <IoIosArrowForward />
