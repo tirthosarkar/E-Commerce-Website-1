6a9330680eea005748ee8c1bf7d6f083ce0c98d5
tirthosarkar <tirthosarkar1@gmail.com>
2026-08-09 00:17:21 +0600

    Added shop.jsx Title

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -10,15 +10,20 @@ const Shop = () => {
       <Layout>
         {/*Breadcrumb*/}
 
-        <div className='max-w-360 lg:px-8 px-5 mx-auto py-5'>
+        <div className="max-w-360 lg:px-8 px-5 mx-auto py-5">
           <div className="flex gap-x-1 items-center text-sm  ">
             <Link to="/">Home</Link>
             <IoIosArrowForward />
 
-            <Link className='font-bold' to="/shop">Shop</Link>
+            <Link className="font-bold" to="/shop">
+              Shop
+            </Link>
           </div>
         </div>
         {/*Title*/}
+        <div className="max-w-360 lg:px-8 px-3 mx-auto py-5">
+          <div className="flex text-4xl text-green-400  ">Shop</div>
+        </div>
         {/*Main Container*/}
       </Layout>
     </>
