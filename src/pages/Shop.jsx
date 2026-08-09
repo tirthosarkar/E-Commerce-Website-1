5ae8a0bfbab393fa48188051435446b587cd8041
tirthosarkar <tirthosarkar1@gmail.com>
2026-08-09 19:41:34 +0600

    Added main containner

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -21,10 +21,28 @@ const Shop = () => {
           </div>
         </div>
         {/*Title*/}
-        <div className="max-w-360 lg:px-8 px-3 mx-auto py-5">
-          <div className="flex text-4xl text-green-400  ">Shop</div>
+        <div className="max-w-360 lg:px-8 px-5 mx-auto">
+          <div className="flex text-4xl text-green-400  ">
+            Shop
+
+          </div>
         </div>
         {/*Main Container*/}
+        <div className="max-w-360 lg:px-8 px-5 mx-auto mt-5">
+          <div className='grid grid-cols-12 gap-5'>
+            {/*Sidebar*/}
+            <div className='col-span-3 border'>
+              asdf
+
+            </div>
+            {/*Product*/}
+            <div className='col-span-9 border'>
+              asdf
+
+            </div>
+
+          </div>
+        </div>
       </Layout>
     </>
   );
