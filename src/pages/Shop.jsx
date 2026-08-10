ac16bdcd235a99b2be1c0c2be8b526b5cb6e3d8a
tirthosarkar <tirthosarkar1@gmail.com>
2026-08-10 21:48:29 +0600

    Added Filter category Brands Price Raqnge

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -28,12 +28,14 @@ const Shop = () => {
         <div className="max-w-360 lg:px-8 px-5 mx-auto mt-5">
           <div className="grid grid-cols-12 gap-5">
             {/*Sidebar*/}
-            <div className="col-span-3 border">
-              <div className="flex justify-between items-center mb-3">
+            <div className="col-span-3 pb-5">
+              <div className="flex justify-between items-center mb-3 border-b border-gray-200">
                 <span className="text-2xl font-bold">Filters</span>
                 <span className="">Clear</span>
               </div>
-              <div className="font-bold text-lg mb-2">Category</div>
+
+              {/*Category filter */}
+              <div className="font-bold pt-2 mb-2">Category</div>
 
               <div className="flex items-center space-x-2">
                 <input type="checkbox" className="w-4 h-4 border-gray-400" />
@@ -42,19 +44,59 @@ const Shop = () => {
 
               <div className="flex items-center space-x-2">
                 <input type="checkbox" className="w-4 h-4 border-gray-400" />
-                <span>Topwear</span>
+                <span>Bottomwear</span>
               </div>
 
               <div className="flex items-center space-x-2">
                 <input type="checkbox" className="w-4 h-4 border-gray-400" />
-                <span>Topwear</span>
-              </div>
-              
-              <div className="flex items-center space-x-2">
-                <input type="checkbox" className="w-4 h-4 border-gray-400" />
-                <span>Topwear</span>
+                <span>Footwear</span>
               </div>
 
+              <div className="flex items-center space-x-2">
+                <input type="checkbox" className="w-4 h-4 border-gray-400" />
+                <span>Shoes</span>
+              </div>
+
+              {/*Brands filter */}
+              <div className="font-bold pt-4 mb-2">Brands</div>
+
+              <div className="flex items-center space-x-2">
+                <input type="checkbox" className="w-4 h-4 border-gray-400" />
+                <span>Allen Solly</span>
+              </div>
+
+              <div className="flex items-center space-x-2">
+                <input type="checkbox" className="w-4 h-4 border-gray-400" />
+                <span>Zara</span>
+              </div>
+
+              <div className="flex items-center space-x-2">
+                <input type="checkbox" className="w-4 h-4 border-gray-400" />
+                <span>Flying Machine</span>
+              </div>
+
+              <div className="flex items-center space-x-2">
+                <input type="checkbox" className="w-4 h-4 border-gray-400" />
+                <span>Jockey</span>
+              </div>
+
+              {/*Price Range filter */}
+              <div className="font-bold pt-4 mb-2">Price Range</div>
+
+              <div className="flex items-center space-x-2">
+                <input type="radio" className="w-4 h-4 border-gray-400" />
+                <span>Under $50</span>
+              </div>
+
+              <div className="flex items-center space-x-2">
+                <input type="radio" className="w-4 h-4 border-gray-400" />
+                <span>$50 - $100</span>
+              </div>
+
+              <div className="flex items-center space-x-2">
+                <input type="radio" className="w-4 h-4 border-gray-400" />
+                <span>Above $100</span>
+              </div>
             </div>
             {/*Product*/}
             <div className="col-span-9 border">asdf</div>
