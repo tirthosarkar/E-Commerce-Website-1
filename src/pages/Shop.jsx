d755a1aae55aea4b06689272945ed5b20a1e6476
tirthosarkar <tirthosarkar1@gmail.com>
2026-08-11 00:52:10 +0600

    Added photo to shop

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -2,9 +2,49 @@
 import Layout from '../components/layouts/Layout'
 import { Link } from 'react-router-dom';
 import { IoIosArrowForward } from 'react-icons/io';
+import ProductImage1 from '../assets/product1.png';
+import ProductImage2 from '../assets/product2.jpg';
+import ProductImage3 from '../assets/product3.png';
 
 
 const Shop = () => {
+  const products = [
+      {
+        id: 1,
+        name: 'Dummy Product 1',
+        image: ProductImage1,
+        price: 100,
+        crossPrice: 110,
+      },
+      {
+        id: 2,
+        name: 'Dummy Product 2',
+        image: ProductImage2,
+        price: 150,
+        crossPrice: 160,
+      },
+      {
+        id: 3,
+        name: 'Dummy Product 3',
+        image: ProductImage3,
+        price: 200,
+        crossPrice: 220,
+      },
+      {
+        id: 4,
+        name: 'Dummy Product 4',
+        image: ProductImage1,
+        price: 250,
+        crossPrice: 270,
+      },
+      {
+        id: 5,
+        name: 'Dummy Product 5',
+        image: ProductImage2,
+        price: 300,
+        crossPrice: 330,
+      },
+    ];
   return (
     <>
       <Layout>
@@ -99,7 +139,32 @@ const Shop = () => {
               </div>
             </div>
             {/*Product*/}
-            <div className="col-span-9 border">asdf</div>
+            <div className="col-span-9 border">
+              <div className="grid grid-cols-1 lg:grid-cols-5 sm:grid-cols-2 space-y-5 lg:space-y-0 gap-5">
+                {products.map(product => {
+                  return (
+                    <div key={product.id}>
+                      <div className="bg-slate-100 overflow-hidden rounded-lg">
+                        <img
+                          className="object-cover h-95 duration-500 ease-in-out transition-transform hover:scale-110"
+                          src={product.image}
+                          alt=""
+                        />
+                      </div>
+                      <h3 className="text-lg text-gray-800 font-semibold">
+                        {product.name}
+                      </h3>
+                      <p className="text-base text-gray-700 font-semibold">
+                        ${product.price}
+                      </p>
+                      <span className="line-through text-gray-500">
+                        ${product.crossPrice}
+                      </span>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
           </div>
         </div>
       </Layout>
