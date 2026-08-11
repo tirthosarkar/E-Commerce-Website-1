1db912d10c355556af4e760de69b9b7cb95050c4
tirthosarkar <tirthosarkar1@gmail.com>
2026-08-11 17:19:55 +0600

    Added Good Product For men

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import Layout from './../components/layouts/Layout';
+import { IoIosArrowForward } from 'react-icons/io';
+import { Link } from 'react-router-dom';
 
 const Product = () => {
   return (
-    <div>Product</div>
-  )
+    <Layout>
+      {/*Breadcrumb*/}
+
+      <div className="max-w-360 lg:px-8 px-5 mx-auto py-5">
+        <div className="flex gap-x-1 items-center text-sm  ">
+          <Link to="/">Shop</Link>
+          <IoIosArrowForward />
+
+          <Link className="font-bold" to="/shop">
+            Good Product for men
+          </Link>
+        </div>
+      </div>
+    </Layout>
+  );
 }
 
 export default Product
