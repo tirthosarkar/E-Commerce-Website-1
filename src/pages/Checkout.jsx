0da9a084c42e1a9910c60890f6ad6024a450032a
tirthosarkar <tirthosarkar1@gmail.com>
2026-08-18 23:35:27 +0600

    Added checkout

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Layout from '../components/layouts/Layout'
 
 function Checkout() {
   return (
-    <div>Checkout</div>
+    <Layout>
+      
+    </Layout>
   )
 }
 
