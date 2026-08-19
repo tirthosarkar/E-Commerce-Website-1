93e7b25811e60c065ed515abb9a81de55571e843
tirthosarkar <tirthosarkar1@gmail.com>
2026-08-19 20:34:18 +0600

    Added Checkout Form name email no. address city state

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -9,7 +9,7 @@ function Checkout() {
        {/*Breadcrumb*/}
 
       <div className="max-w-360 lg:px-8 px-5 mx-auto py-5">
-        <div className="flex gap-x-1 items-center text-sm  ">
+        <div className="flex gap-x-1 items-center text-sm">
           <Link to="/">Home</Link>
           <IoIosArrowForward />
 
@@ -27,7 +27,7 @@ function Checkout() {
         <div className='grid grid-cols-12 gap-5'>
           {/**Billing Form */}
 <div className='col-span-8'>
-  <div className='shadow-lg rounded-md px-6 py-5 border border-gray-100 '>
+  <div className='shadow-lg rounded-md px-6 py-5 border border-gray-100'>
 <h2 className='text-2xl font-semibold pb-3 border-b border-gray-200 mb-3'>Billing Details</h2>
 <form action="">
   <div>
@@ -47,7 +47,7 @@ placeholder='Enter Name'
 
     <div>
       <input 
-    placeholder='Enter No.'
+    placeholder='Enter Phone No.'
     type="text" 
     className='border border-gray-200 px-3 py-3 w-full rounded-md' />
 </div>
@@ -60,6 +60,22 @@ placeholder='Enter Name'
       
     </textarea>
 </div>
+
+    <div className='grid grid-cols-2 gap-3'>
+<div>
+      <input 
+    placeholder='City'
+    type="text" 
+    className='border border-gray-200 px-3 py-3 w-full rounded-md' />
+</div>
+
+    <div>
+      <input 
+    placeholder='State'
+    type="text" 
+    className='border border-gray-200 px-3 py-3 w-full rounded-md' />
+</div>
+  </div>
 
 </form>
   </div>
