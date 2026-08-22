a1656e8b1afa64cda42467745a056c6f4235188c
tirthosarkar <tirthosarkar1@gmail.com>
2026-08-22 16:34:24 +0600

    Added more order summary size text mb 3

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -94,7 +94,7 @@ placeholder=' Zipcode'
   <div className='rounded-md bg-gray-50 p-5 shadow-md'>
     <h2 className='text-2xl text-gray-800 font-semibold pb-3 border-b border-gray-200 mb-3'>Order Summary</h2>
 
-<div className='grid grid-cols-12 gap-5'>
+<div className='grid grid-cols-12 gap-5 mb-3'>
   {/**Image */}
   <div className='col-span-2'>
     <img src={ProductImg} className='w-full rounded-md bg-slate-50' alt="" />
@@ -113,11 +113,37 @@ placeholder=' Zipcode'
   </div>
 
   {/**price */}
-<div className='col-span-2 text-sm'>
+<div className='col-span-2 text-sm font-semibold'>
 $100
 </div>
 
 </div>
+
+<div className='grid grid-cols-12 gap-5 mb-3'>
+  {/**Image */}
+  <div className='col-span-2'>
+    <img src={ProductImg} className='w-full rounded-md bg-slate-50' alt="" />
+  </div>
+
+  {/**title, size, qty */}
+  <div className='col-span-8'>
+    {/**title */}
+<h3 className='text-gray-700 text-sm font-semibold'>
+  Nice white jacked for women
+  </h3>
+    <div className='flex space-x-5 text-sm'>
+  <div className='text-gray-600'>Size <span className='text-gray-800'>L</span></div>
+  <div className='text-gray-600'>Qty <span className='text-gray-800'>2</span></div>
+  </div>
+  </div>
+
+  {/**price */}
+<div className='col-span-2 text-sm font-semibold'>
+$100
+</div>
+
+</div>
+
   </div>
 </div>
         </div>
