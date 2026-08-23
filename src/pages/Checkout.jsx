3630dff08114cee6110b17403248a5e688875624
tirthosarkar <tirthosarkar1@gmail.com>
2026-08-23 23:59:44 +0600

    Added checkeout radio button bg px font text py w-full hover bg then text white data di  text

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -168,7 +168,7 @@ $100
   Place Order Securely
 </button>
 
-<div>Secure payment & encrypted checkout</div>
+<div className='text-gray-400'>Secure payment & encrypted checkout</div>
   </div>
 </div>
         </div>
