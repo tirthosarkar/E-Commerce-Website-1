ee5578b582c161705beb26a92639e3c601e5fb34
tirthosarkar <tirthosarkar1@gmail.com>
2026-08-23 23:48:29 +0600

    Added checkeout radio button bg px font

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -163,7 +163,7 @@ $100
     <span>Stripe</span>
   </div>
 </div>
-<button className='bg-green-400 px-5'>
+<button className='bg-green-400 px-5 font-semibold'>
   Place Order Securely
 </button>
   </div>
