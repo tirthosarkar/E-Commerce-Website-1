3a2c77f57947a007f963a251b9ef14e7a8da1802
tirthosarkar <tirthosarkar1@gmail.com>
2026-08-24 00:32:22 +0600

    Added Account Profile Breadcrumb

--- src/pages/account/Profile.jsx
+++ src/pages/account/Profile.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
 import Layout from '../../components/layouts/Layout'
+import { Link } from 'react-router-dom'
+import { IoIosArrowForward } from 'react-icons/io'
 
 const Profile = () => {
   return (
     <Layout>
       
+       {/*Breadcrumb*/}
+
+      <div className="max-w-360 lg:px-8 px-5 mx-auto py-5">
+        <div className="flex gap-x-1 items-center text-sm  ">
+          <Link to="/">Account</Link>
+          <IoIosArrowForward />
+
+          <Link className="font-bold" to="/shop">
+            Profile
+          </Link>
+        </div>
+      </div>
+      {/*Title*/}
+      <div className="max-w-360 lg:px-8 px-5 mx-auto">
+        <div className="flex text-4xl text-green-400  ">Profile</div>
+      </div>
+
+
+
     </Layout>
   )
 }
