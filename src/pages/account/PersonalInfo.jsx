1c960e76f4ae1c2df67b757ae20dcc75038e3526
tirthosarkar <tirthosarkar1@gmail.com>
2026-08-24 14:42:32 +0600

    Added Account Profile Breadcrumb sidebar Personal Information button

---
+++ src/pages/account/PersonalInfo.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+
+const PersonalInfo = () => {
+  return (
+    <>
+      <div className='shadow-lg rounded-md'>
+    <h2 className='bg-gray-100 px-5 py-2 rounded-t-md 
+    text-lg font-semibold'>Personal Information</h2>
+
+    <form className='space-y-4 py-3 px-5'>
+        <div className='flex flex-col'>
+            <label htmlFor="" className='text-lg mb-2'>Name</label>
+             <input
+                
+                type="text"
+                placeholder="Enter Name"
+                className="px-3 py-2 border border-gray-200 shadow rounded-md"
+              />
+        </div>
+        <div className='flex flex-col'>
+            <label htmlFor="" className='text-lg mb-2'>Email</label>
+             <input
+                
+                type="text"
+                placeholder="Enter Email"
+                className="px-3 py-2 border border-gray-200 shadow rounded-md"
+              />
+        </div>
+        <div className='flex flex-col'>
+            <label htmlFor="" className='text-lg mb-2'>Phone No.</label>
+             <input
+                
+                type="text"
+                placeholder="Enter Phone No."
+                className="px-3 py-2 border border-gray-200 shadow rounded-md"
+              />
+        </div>
+        <div className='py-3'>
+             <button className="bg-green-400 px-5 py-3  text-md rounded-md text-gray-800 text-center hover:text-white hover:bg-gray-900">
+                            Update
+                          </button>
+        </div>
+
+    </form>
+</div>
+    </>
+  )
+}
+
+export default PersonalInfo
--- src/pages/account/Profile.jsx
+++ src/pages/account/Profile.jsx
@@ -3,6 +3,7 @@ import Layout from '../../components/layouts/Layout'
 import { Link } from 'react-router-dom'
 import { IoIosArrowForward } from 'react-icons/io'
 import Sidebar from './Sidebar'
+import PersonalInfo from './PersonalInfo'
 
 
 
@@ -37,46 +38,7 @@ const Profile = () => {
 </div>
 {/* Form Area */}
 <div className='col-span-9'>
-<div className='shadow-lg rounded-md'>
-    <h2 className='bg-gray-100 px-5 py-2 rounded-t-md 
-    text-lg font-semibold'>Personal Information</h2>
-
-    <form className='space-y-4 py-3 px-5'>
-        <div className='flex flex-col'>
-            <label htmlFor="" className='text-lg mb-2'>Name</label>
-             <input
-                
-                type="text"
-                placeholder="Enter Name"
-                className="px-3 py-2 border border-gray-200 shadow rounded-md"
-              />
-        </div>
-        <div className='flex flex-col'>
-            <label htmlFor="" className='text-lg mb-2'>Email</label>
-             <input
-                
-                type="text"
-                placeholder="Enter Email"
-                className="px-3 py-2 border border-gray-200 shadow rounded-md"
-              />
-        </div>
-        <div className='flex flex-col'>
-            <label htmlFor="" className='text-lg mb-2'>Phone No.</label>
-             <input
-                
-                type="text"
-                placeholder="Enter Phone No."
-                className="px-3 py-2 border border-gray-200 shadow rounded-md"
-              />
-        </div>
-        <div className='py-3'>
-             <Link className="bg-green-400 px-5 py-3  text-md rounded-md text-gray-800 text-center hover:text-white hover:bg-gray-900">
-                            Update
-                          </Link>
-        </div>
-
-    </form>
-</div>
+<PersonalInfo/>
 
 </div>
 
